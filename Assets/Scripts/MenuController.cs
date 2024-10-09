a50d8b0f6f82236e6dc9e694e9a826c0d5a8b4b2
ThibPltt <thibautplatet@orange.fr>
2024-10-09 23:41:37 +0200

    V10

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,6 +7,6 @@ public class MenuController : MonoBehaviour
 {
     public void StartButton()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainScene");
     }
 }
